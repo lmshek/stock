16ef6ca1af83eabb8790976a67393a84aa40dd1b
sheklokman <sheklokman@gmail.com>
2023-02-03 09:14:27 +0800

    fix bug

--- stockfinder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export API_TOKEN=6065972502:AAEidJXRtnouJT27vL0npxM4Mr34PkqAGI0
-export CHAT_ID=@rockysuck_stock_channel
-cd /Users/lmshek/Documents/GitHubExternal/stock ; /usr/bin/env /Users/lmshek/opt/anaconda3/envs/testing/bin/python stockfinder.py
