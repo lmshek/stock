2fd8124aede597100ed5a56705ba31e3a8136ee1
sheklokman <lmshek@cityu.edu.hk>
2023-08-16 15:38:17 +0800

    update forex market

---
+++ sh/stockfinder_FOREX.sh
@@ -0,0 +1,7 @@
+cd /Users/lmshek/Documents/GitHubExternal/stock
+if [ ! -f .env ]
+then
+  export $(cat .env | xargs)
+fi
+
+cd /Users/lmshek/Documents/GitHubExternal/stock & /usr/bin/env /Users/lmshek/opt/anaconda3/envs/testing/bin/python stockfinder_technical_breakout.py --market FOREX --stock_list forex
--- sh/stockfinder_HK.sh
+++ sh/stockfinder_HK.sh
@@ -4,4 +4,4 @@ then
   export $(cat .env | xargs)
 fi
 
-cd /Users/lmshek/Documents/GitHubExternal/stock & /usr/bin/env /Users/lmshek/opt/anaconda3/envs/testing/bin/python stockfinder_technical_breakout.py --market HK --stock_list forex hsi_integrated_large hsi_integrated_medium hsi_integrated_small
+cd /Users/lmshek/Documents/GitHubExternal/stock & /usr/bin/env /Users/lmshek/opt/anaconda3/envs/testing/bin/python stockfinder_technical_breakout.py --market HK --stock_list hsi_integrated_large hsi_integrated_medium hsi_integrated_small
